14b65a59a3e01901e477827f3d3b65903845f9cf
She Khang <shekhang.kiang@gmail.com>
2020-10-10 06:17:50 +0800

    add Login page register and page one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
+
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path:'',
+    redirectTo:'login',
+    pathMatch:'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'page-one',
+    loadChildren: () => import('./dashboard/page-one/page-one.module').then( m => m.PageOnePageModule)
+  },
+  {
+    path: 'page-two',
+    loadChildren: () => import('./dashboard/page-two/page-two.module').then( m => m.PageTwoPageModule)
   }
 ];
 @NgModule({
