184770f3914c6c1c5deb2f58adaa368ded1b72a6
She Khang <shekhang.kiang@gmail.com>
2020-10-10 17:32:40 +0800

    other changes

--- src/app/dashboard/settings/settings.page.ts
+++ src/app/dashboard/settings/settings.page.ts
@@ -1,3 +1,4 @@
+import { PreferensiResponseData } from './../../auth/auth-response';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { Storage } from  '@ionic/storage';
@@ -11,7 +12,7 @@ import { Router } from  "@angular/router";
 export class SettingsPage implements OnInit {
 
   userName : string = "TDWG";
-  preferensi : Array<string> = [];
+  preferensi : Array<PreferensiResponseData> = [];
 
   constructor(private  authService:  AuthService, private  storage:  Storage, private  router:  Router) { }
 
